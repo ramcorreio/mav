dc48f365a372d12cce3c71a0fcfe270130010e10
Rodrigo Macedo <rodrigo.macedo@datagrupo.com.br>
2015-10-29 23:45:45 -0200

    implemetação da porta funcionando

--- mav-core/src/main/java/com/stefanini/mav/message/MessageIoHandler.java
+++ mav-core/src/main/java/com/stefanini/mav/message/MessageIoHandler.java
@@ -1,71 +1,48 @@
 package com.stefanini.mav.message;
 
-import java.awt.image.BufferedImage;
+import java.util.Date;
 
 import org.apache.mina.core.service.IoHandlerAdapter;
+import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.core.session.IoSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.stefanini.mav.tcp.MessageRequest;
-
 public class MessageIoHandler extends IoHandlerAdapter {
 
-    //private final static String characters = "mina rocks abcdefghijklmnopqrstuvwxyz0123456789";
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    public static final String INDEX_KEY = MessageIoHandler.class.getName() + ".INDEX";
-
-    private Logger logger = LoggerFactory.getLogger(this.getClass());
-
-    public void sessionOpened(IoSession session) throws Exception {
-        //session.setAttribute(INDEX_KEY, 0);
-    	System.out.println(session);
+	
+	@Override
+    public void exceptionCaught( IoSession session, Throwable cause ) throws Exception
+    {
+        logger.error("session: " + session.getId(), cause);
     }
-
-    public void exceptionCaught(IoSession session, Throwable cause) throws Exception {
-        //IoSessionLogger sessionLogger = IoSessionLogger.getLogger(session, logger);
-    	//sessionLogger.warn(cause.getMessage(), cause);
-    	System.out.println(session);
-    	System.out.println(cause);
+	
+	@Override
+    public void sessionIdle( IoSession session, IdleStatus status ) throws Exception
+    {
+		logger.info( "IDLE " + session.getIdleCount( status ));
     }
+	
+	@Override
+	public void messageReceived(IoSession session, Object message) throws Exception {
 
-    public void messageReceived(IoSession session, Object message) throws Exception {
-        /*ImageRequest request = (ImageRequest) message;
-        String text1 = generateString(session, request.getNumberOfCharacters());
-        String text2 = generateString(session, request.getNumberOfCharacters());
-        BufferedImage image1 = createImage(request, text1);
-        BufferedImage image2 = createImage(request, text2);
-        ImageResponse response = new ImageResponse(image1, image2);
-        session.write(response);*/
-    }
+		System.out.println("Message class: " + message.getClass());
+		String str = message.toString();
+		System.out.println("Message received: " + str);
+		if (str.trim().equalsIgnoreCase("quit")) {
+			session.close(true);
+			return;
+		}
+		
+		if (str.trim().equals("data")) {
+			Date date = new Date();
+			session.write(date.toString()); /* Retornando dado ao cliente */
+		} else {
+			session.write("echo " + str); /* Retornando dado ao cliente */
+		}
 
-    private BufferedImage createImage(MessageRequest request, String text) {
-        /*BufferedImage image = new BufferedImage(request.getWidth(), request.getHeight(), BufferedImage.TYPE_BYTE_INDEXED);
-        Graphics graphics = image.createGraphics();
-        graphics.setColor(Color.YELLOW);
-        graphics.fillRect(0, 0, image.getWidth(), image.getHeight());
-        Font serif = new Font("serif", Font.PLAIN, 30);
-        graphics.setFont(serif);
-        graphics.setColor(Color.BLUE);
-        graphics.drawString(text, 10, 50);
-        return image;*/
-    	return null;
-    }
-
-    /*private String generateString(IoSession session, int length) {
-    	
-        Integer index = (Integer) session.getAttribute(INDEX_KEY);
-        StringBuffer buffer = new StringBuffer(length);
-
-        while (buffer.length() < length) {
-            buffer.append(characters.charAt(index));
-            index++;
-            if (index >= characters.length()) {
-                index = 0;
-            }
-        }
-        session.setAttribute(INDEX_KEY, index);
-        return buffer.toString();
-    }*/
+	}
 
 }
--- mav-core/src/main/java/com/stefanini/mav/startup/MessageServerBootstrap.java
+++ mav-core/src/main/java/com/stefanini/mav/startup/MessageServerBootstrap.java
@@ -2,54 +2,77 @@ package com.stefanini.mav.startup;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.charset.Charset;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 
+import org.apache.mina.core.session.IdleStatus;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.filter.codec.textline.TextLineCodecFactory;
+import org.apache.mina.filter.logging.LoggingFilter;
 import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.stefanini.mav.message.MessageIoHandler;
 
+
+
 public class MessageServerBootstrap {
-	
+
+	//TODO: verificar como configurar timeout
+	//private static int TIMEOUT = 1000;
+
 	private static int PORT = 8889;
 	
-	private static MessageServerBootstrap server;
-	
-	private Logger logger = LoggerFactory.getLogger(MessageServerBootstrap.class);
-	
+	protected ClassLoader loader;
+
+	private static MessageServerBootstrap deamon = null;
+
+	private static Logger logger = LoggerFactory.getLogger(MessageServerBootstrap.class);
+
 	private NioSocketAcceptor acceptor;
-	
+
 	public void init() throws IOException {
+
+		acceptor = new NioSocketAcceptor();
 		
-		MessageIoHandler handler = new MessageIoHandler();
-        acceptor = new NioSocketAcceptor();
-        //TODO: verificar a necessidade da linha abaixo
-        //acceptor.getFilterChain().addLast("protocol", new ProtocolCodecFilter(new ImageCodecFactory(false)));
-        acceptor.setDefaultLocalAddress(new InetSocketAddress(PORT));
-        acceptor.setHandler(handler);
-        acceptor.bind();
-        logger.info("server is listenig at port " + PORT);
+		acceptor.getFilterChain().addLast( "logger", new LoggingFilter() );
+		acceptor.getFilterChain().addLast( "codec", new ProtocolCodecFilter( new TextLineCodecFactory( Charset.forName( "UTF-8" ))));
+		
+		acceptor.setHandler(new MessageIoHandler());
+		acceptor.getSessionConfig().setReadBufferSize(2048);
+		acceptor.getSessionConfig().setIdleTime(IdleStatus.BOTH_IDLE, 10);
+
+		acceptor.setDefaultLocalAddress(new InetSocketAddress(PORT));
+		acceptor.bind();
+		logger.info("server is listenig at port " + PORT);
 	}
-	
+
 	private void stop() {
-		
+
 		acceptor.setCloseOnDeactivation(true);
 		acceptor.unbind();
 	}
-	
-	public static void main(String[] args) throws IOException {
-		
-		
-		if(server == null) {
-		
-			server = new MessageServerBootstrap();
-			server.init();
-		}
-		else if(args.length > 0) {
+
+	public static void main(String[] args) throws IOException, URISyntaxException {
+
+		if (deamon == null) {
+
+			MessageServerBootstrap bootstrap = new MessageServerBootstrap();
+			bootstrap.init();
+			logger.info("loader: " + bootstrap.loader);
 			
-			server.stop();
+			deamon = bootstrap;
+			
+		} else if (args.length > 0) {
+
+			deamon.stop();
 		}
-		
-		server.logger.info("server already listenig at port " + PORT);
+
+		//server.logger.info("server already listenig at port " + PORT);
 	}
 }
