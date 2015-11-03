4455dead743f77206430007d65883cfab3d54f49
Rodrigo Macedo <rodrigo.macedo@datagrupo.com.br>
2015-11-03 02:07:30 -0200

    preparação dos métodos de testes para implementar

--- mav-core/src/test/java/com/stefanini/mav/mensagem/MensagemFactoryTest.java
+++ mav-core/src/test/java/com/stefanini/mav/mensagem/MensagemFactoryTest.java
@@ -8,7 +8,16 @@ import org.junit.Test;
 
 import com.stefanini.mav.util.MensagemHelper;
 
+/**
+ * 
+ * Layout das mensagens está mav/mav-core/docs/TRS_CDC-EP_V9-Layout_Completo_082015.xls
+ * 
+ * @author Rodrigo
+ *
+ */
 public class MensagemFactoryTest {
+	
+	
 	
 	@Test
 	public void getNumTransacao() {
@@ -29,14 +38,43 @@ public class MensagemFactoryTest {
 	}
 	
 	@Test
-	public void criarMensagemSimplificada450() throws IOException, URISyntaxException, MensagemNaoEncontradaException{
+	public void criarCapturaSimplicada() throws IOException, URISyntaxException, MensagemNaoEncontradaException{
 		
-		String messagem = MensagemHelper.lerMensagem(186, 450, "criarMensagemSimplificada450.1");
+		String messagem = MensagemHelper.lerMensagem(186, 450, "criarCapturaSimplicada.1");
 		
 		MensagemBasica m = MensagemFactory.parse(messagem);
 		Assert.assertEquals(TipoMensagem.C0450, m.getCabecalho().getTipo());
 		Assert.assertEquals(Integer.valueOf(116), m.getCabecalho().getTamanho());
 		
 	}
-
+	
+	@Test
+	public void criarRespostaCapturaSimplicada() {
+		
+		Assert.fail("Não implementado.");
+	}
+	
+	@Test
+	public void criarGeracaoToken() {
+		
+		Assert.fail("Não implementado.");
+	}
+	
+	@Test
+	public void criarRespostaGeracaoToken() {
+		
+		Assert.fail("Não implementado.");
+	}
+	
+	@Test
+	public void criarPropostaFinanciamento() {
+		
+		Assert.fail("Não implementado.");
+	}
+	
+	@Test
+	public void criarRespostaPropostaFinanciamento() {
+		
+		Assert.fail("Não implementado.");
+	}
 }
