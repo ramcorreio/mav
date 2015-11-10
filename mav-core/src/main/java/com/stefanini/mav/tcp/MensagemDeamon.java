7190cebdb809f1cb8b111ce73d32853f5c2d73f5
Rodrigo Macedo <rodrigo.macedo@datagrupo.com.br>
2015-11-10 02:15:04 -0200

    Acerto no nome da classe servidora

--- mav-core/src/main/java/com/stefanini/mav/tcp/MensagemDeamon.java
+++ mav-core/src/main/java/com/stefanini/mav/tcp/MensagemDeamon.java
@@ -5,22 +5,23 @@ import java.net.InetSocketAddress;
 
 import org.apache.mina.core.filterchain.IoFilterChainBuilder;
 import org.apache.mina.core.service.IoHandler;
+import org.apache.mina.core.service.IoServiceListener;
 import org.apache.mina.core.session.IdleStatus;
 import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class MensagemListener {
+public class MensagemDeamon {
 
 	private static int DEFAULT_PORT = 8889;
 	
-	private static Logger logger = LoggerFactory.getLogger(MensagemListener.class);
+	private static Logger logger = LoggerFactory.getLogger(MensagemDeamon.class);
 
 	private NioSocketAcceptor acceptor;
 	
 	private int port = DEFAULT_PORT; 
 	
-	public MensagemListener() {
+	public MensagemDeamon() {
 		
 		acceptor = new NioSocketAcceptor();
 		acceptor.getSessionConfig().setReadBufferSize(2048);
@@ -40,6 +41,11 @@ public class MensagemListener {
 	public void setHandler(IoHandler handler) {
 		
 		acceptor.setHandler(handler);
+	}
+	
+	public void addListener(IoServiceListener listener) {
+		
+		acceptor.addListener(listener);
 	}
 	
 	public void start() throws IOException {
