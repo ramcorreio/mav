d5dbb1e6bfae737d79efcf15e9a0227b27110abd
Rodrigo Macedo <rodrigo.macedo@datagrupo.com.br>
2016-03-02 20:49:17 -0300

    implementação de interface para interagir com o mav

--- mav-web/src/main/java/MensagemView.java
+++ mav-web/src/main/java/MensagemView.java
@@ -1,23 +1,42 @@
+import java.io.Serializable;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 @ManagedBean
 @ViewScoped
-public class MensagemView {
+public class MensagemView implements Serializable {
 	
-	private String string;
+	private static final long serialVersionUID = 6137507973393064760L;
+
+	private String input;
 	
-	public String getString() {
-		return string;
+	private String result;
+	
+	public String getInput() {
+		return input;
 	}
 	
-	public void setString(String string) {
-		this.string = string;
+	public void setInput(String input) {
+		this.input = input;
+	}
+	
+	public String getResult() {
+		return result;
+	}
+	
+	public void setResult(String result) {
+		this.result = result;
 	}
 	
 	public void enviar() {
 		
-		System.out.println("enviando: " + string);
+		System.out.println("enviando: " + input);
+		StringBuilder b = new StringBuilder();
+		b.append("entrada: " + input);
+		b.append("<br/>======================================<br/>");
+		b.append("saída: " + input);
+		result = b.toString();
 	}
 
 }
